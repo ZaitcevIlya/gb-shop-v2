7bd2ed2bbc5a95b66d8b0f2d0599699d25b22da8
ZaitcevIlya <izaytsev.work@gmail.com>
2022-01-29 15:38:12 +0300

    preparation for lesson 5

--- ordersapp/views.py
+++ ordersapp/views.py
@@ -1,6 +1,6 @@
 from django.db import transaction
 from django.forms import inlineformset_factory
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
@@ -8,6 +8,7 @@ from django.shortcuts import get_object_or_404
 
 from baskets.models import Basket
 from mainapp.mixin import BaseClassContextMixin
+from mainapp.models import Product
 from ordersapp.forms import OrderItemsForm
 from ordersapp.models import Order, OrderItem
 
@@ -119,3 +120,14 @@ def order_forming_complete(request, pk):
     order.save()
 
     return HttpResponseRedirect(reverse('orders:list'))
+
+
+def get_product_price(request, pk):
+    if request.is_ajax():
+        product = Product.objects.get(id=pk)
+        print(product)
+
+        if product:
+            return JsonResponse({'price': product.price})
+
+        return JsonResponse({'price': 0})
