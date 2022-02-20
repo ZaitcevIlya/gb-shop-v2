dac2ab93bbe324d6eaf9b6f5d831c8f7ce53b7b2
ZaitcevIlya <izaytsev.work@gmail.com>
2022-02-20 19:14:05 +0300

    fix login form

--- authapp/views.py
+++ authapp/views.py
@@ -23,7 +23,7 @@ class LoginListView(LoginView,BaseClassContextMixin):
     # def get(self, request, *args, **kwargs):
     #     if request.user.is_authenticated:
     #         return HttpResponseRedirect(reverse('index'))
-    #     return HttpResponseRedirect(reverse('authapp:login'))
+    #     return HttpResponseRedirect(reverse('users:login'))
 
 
 class RegisterListView(FormView,BaseClassContextMixin):
