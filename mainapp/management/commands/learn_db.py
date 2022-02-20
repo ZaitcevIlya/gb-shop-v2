161b86936e90ab423110100795696338b3ae4d2e
ZaitcevIlya <izaytsev.work@gmail.com>
2022-02-20 17:17:11 +0300

    add discount and script

---
+++ mainapp/management/commands/learn_db.py
@@ -0,0 +1,19 @@
+from time import time
+
+from django.core.management.base import BaseCommand
+from mainapp.models import Product
+from django.db.models import Q
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        # products = Product.objects.filter(
+        #    Q(category__name='Обувь') & Q(id=17))
+        # products = Product.objects.filter(
+        #     Q(category__name='Обувь') & Q(id=5))
+        # products = Product.objects.filter(
+        #     ~Q(category__name='Обувь'))
+        products = Product.objects.filter(
+            ~Q(category__name='Обувь'), id=13)
+
+        print(products)
